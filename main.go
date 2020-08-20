bf09f736a98fd6f10c070fc5c8763af10bbda29e
gwuah <griffithawuah15@gmail.com>
2020-08-20 17:19:29 +0000

    filtering done

--- main.go
+++ main.go
@@ -4,7 +4,12 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
+
+func processFile(path string) bool {
+	return true
+}
 
 func main() {
 	fmt.Println("[PASTA]")
@@ -15,10 +20,32 @@ func main() {
 	// filter all snapshot files (.spec.tsx.snap)
 
 	var files []string
+	var components []string
+	var testFiles []string
+	var snapshots []string
+	var blackList = map[string]bool{
+		"/Users/user/Desktop/work/petra/web/src/setupTests.js":      true,
+		"/Users/user/Desktop/work/petra/web/src/serviceWorker.ts":   true,
+		"/Users/user/Desktop/work/petra/web/src/react-app-env.d.ts": true,
+	}
+
 	pathToCodebase := "/Users/user/Desktop/work/petra/web/src"
 
 	err := filepath.Walk(pathToCodebase, func(path string, info os.FileInfo, err error) error {
-		files = append(files, path)
+		if _, ok := blackList[path]; ok {
+			return nil
+		}
+
+		if strings.HasSuffix(path, ".spec.tsx") {
+			testFiles = append(testFiles, path)
+		} else if strings.HasSuffix(path, ".spec.tsx.snap") {
+			snapshots = append(snapshots, path)
+		} else if strings.HasSuffix(path, ".tsx") || strings.HasSuffix(path, ".ts") {
+			components = append(components, path)
+		} else {
+			files = append(files, path)
+		}
+
 		return nil
 	})
 
@@ -26,6 +53,12 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(files)
+	// fmt.Println(testFiles)
+	// fmt.Println(components)
+	// fmt.Println(snapshots)
+
+	for _, file := range components {
+		fmt.Println(file)
+	}
 
 }
