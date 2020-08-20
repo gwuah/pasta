f23b34a5e33b04a7dc98be36fd10f1c285c86458
gwuah <griffithawuah15@gmail.com>
2020-08-20 17:02:00 +0000

    find all files

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	fmt.Println("hello")
+	fmt.Println("[PASTA]")
+
+	// find all files in repo
+	// find all test files (.spec.tsx)
+	// filter out actual typescript files (.tsx)
+	// filter all snapshot files (.spec.tsx.snap)
+
+	var files []string
+	pathToCodebase := "/Users/user/Desktop/work/petra/web/src"
+
+	err := filepath.Walk(pathToCodebase, func(path string, info os.FileInfo, err error) error {
+		files = append(files, path)
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(files)
+
 }
